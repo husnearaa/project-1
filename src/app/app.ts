6a85442bc222d04347f941acabb4187f4beedc0c
husnearaa <hosnearapopy462@gmail.com>
2024-12-31 01:58:11 +0600

    has update code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,71 @@
-import express  from 'express'
-const app = express()
-const port = 3000
+// import express, { Request, Response }  from 'express'
 
-app.get('/', (req, res) => {
-  res.send('Hello Developers!')
+// const app = express();
+// // import cookieParser from "cookie-parser";
+// const port = 3000;
+
+
+// //parsers
+// app.use(express.json());
+// // app.use(cookieParser());
+
+
+// app.get('/', (req: Request, res: Response) => {
+//   res.send('Hello Developers!')
+// });
+
+// app.post("/product", (req: Request, res: Response) =>{
+//   // console.log(req.body);
+//   res.send({
+//     message:"Success"
+//   });
+// });
+
+
+// // app.post("/notfound", (req, res) => {
+// //   console.log("hello");
+// //   try {
+   
+// //     res.send({
+// //       message: "Success"
+// //     });
+// //   } catch (error) {
+// //     console.error(error);
+// //     res.status(500).send({ message: "Internal Server Error" });
+// //   }
+// // });
+
+
+// export default app;
+
+
+
+
+
+
+import express, { Request, Response } from "express"
+const app = express() 
+
+// parser
+app.use(express.json())
+
+
+
+app.post("/product",async(req:Request,res:Response)=>{
+
+  console.log("data")
+
+  res.send(req.body)
+
 })
 
-export default app;
+
+app.get('/', async(req:Request, res:Response) => {
+  res.send('Hello World Now 5!')
+})
+
+
+
+
+
+export default app
---
+++ src/app/extraFile.js
@@ -0,0 +1,19 @@
+/****
+ * 
+ * 
+ * to do run typescript:
+ * "tsc"  command
+ * node ./dist/app/server.js
+ * 
+ * 
+ * "tsc -w" by this command we change the ts file updated code in to js file.
+ * 
+ * and to run server always without hassle free need to install nodemon
+ * 
+ * "npm add -D nodemon"
+ * 
+ * after installing this nodemon have to run by this command
+ * "npm run startDev"  // because i have added the script this way in the package.json file
+ * "startDev":"nodemon ./dist/app/server.js",
+ * 
+ */
